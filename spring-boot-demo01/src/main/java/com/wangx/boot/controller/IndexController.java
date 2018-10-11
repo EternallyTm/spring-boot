716e69877a3fc9bf517e12a205aa917f4a1e91bf
EternallyTm <1028106567@qq.com>
2018-10-11 22:59:30 +0800

    SpringBoot Web开发FreeMarker前后台数据交互

--- spring-boot-demo01/src/main/java/com/wangx/boot/controller/IndexController.java
+++ spring-boot-demo01/src/main/java/com/wangx/boot/controller/IndexController.java
@@ -3,9 +3,6 @@ package com.wangx.boot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -14,33 +11,8 @@ public class IndexController {
 
     @RequestMapping("/index")
         public String hello(Map<String,Object> map){
-
+            //传递数据到freemaker模板中
             map.put("name", "[Angel -- 守护天使]");
-
-            map.put("gender",1);//gender:性别，1：男；0：女；
-
-
-
-            List<Map<String,Object>> friends =new ArrayList<Map<String,Object>>();
-
-            Map<String,Object> friend = new HashMap<String,Object>();
-
-            friend.put("name", "张三");
-
-            friend.put("age", 20);
-
-            friends.add(friend);
-
-            friend = new HashMap<String,Object>();
-
-            friend.put("name", "李四");
-
-            friend.put("age", 22);
-
-            friends.add(friend);
-
-            map.put("friends", friends);
-
             return "index";
 
     }
--- spring-boot-demo01/src/main/resources/templates/index.ftl
+++ spring-boot-demo01/src/main/resources/templates/index.ftl
@@ -13,45 +13,11 @@
 <body>
 
 <p>
-    <img src="/1.jpg">
+    <!--获取后台传过来的数据-->
+    <p>${name}</p>
 </p>
 
-
-
-
-
-<p>性别：
-
-           <#if gender==0>
-
-              女
-
-           <#elseif gender==1>
-
-              男
-
-           <#else>
-
-              保密
-
-           </#if>
-
 </p>
-
-
-
-
-
-<h4>我的好友：</h4>
-
-       <#list friends as item>
-
-           姓名：${item.name} , 年龄${item.age}
-
-           <br>
-
-       </#list>
-
 
 
 </body>
