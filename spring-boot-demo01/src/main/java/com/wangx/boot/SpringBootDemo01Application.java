8069e0e6490ee17c282a64f4d62c78facc520e29
EternallyTm <1028106567@qq.com>
2018-10-11 21:46:24 +0800

    SpringBoot整合FreeMarker

--- spring-boot-demo01/src/main/java/com/wangx/boot/SpringBootDemo01Application.java
+++ spring-boot-demo01/src/main/java/com/wangx/boot/SpringBootDemo01Application.java
@@ -1,4 +1,4 @@
-package com.wangx.boot.springbootdemo01;
+package com.wangx.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-demo01/src/test/java/com/wangx/boot/SpringBootDemo01ApplicationTests.java
+++ spring-boot-demo01/src/test/java/com/wangx/boot/SpringBootDemo01ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wangx.boot.springbootdemo01;
+package com.wangx.boot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
